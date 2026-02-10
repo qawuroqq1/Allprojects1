87a31f28a8ede901b9ace7492ce707fdd99e4133
Данияр Макашев <danik.makashev@bk.ru>
2026-02-10 16:22:21 +0500

    first commit

---
+++ AllProjects/ApiGateway-reworked-5-week/ApiGateway/FileName.cs
@@ -0,0 +1,6 @@
+﻿namespace ApiGateway
+{
+    public class FileName
+    {
+    }
+}
---
+++ AllProjects/ApiGateway-reworked-5-week/ApiGateway/Program.cs
@@ -0,0 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddHealthChecks();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+await app.UseOcelot();
+
+app.Run();
